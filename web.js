5a6bec5b678d405b7b2807c862a747cd759f9efe
srikumars <srikumars@gmail.com>
2013-07-13 05:29:15 +0000

    edited to read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
+var fs = require('fs');
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var outfile = "index.html";
+  var out;
+  out = fs.readFileSync(infile)
+
+  response.send(out.toString());
 });
 
 var port = process.env.PORT || 5000;
